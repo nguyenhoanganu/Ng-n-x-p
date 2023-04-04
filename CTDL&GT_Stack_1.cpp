92803687ba89b5554741519d6b82a46dbea80a88
nguyenhoanganu <129148984+nguyenhoanganu@users.noreply.github.com>
2023-04-04 17:12:52 +0700

    Create CTDL&GT_Stack_1.cpp

---
+++ CTDL&GT_Stack_1.cpp
@@ -0,0 +1,33 @@
+bool IsEmpty(){
+    if(top == -1){
+        return true;
+    }else{
+        return false;
+    }
+}
+ 
+void Push(int stack[], int value, int capacity){
+    if(IsFull(capacity) == true){
+        printf("\nStack is full. Overflow condition!");
+    }else{
+        ++top;
+        stack[top] = value;
+    }
+}
+ 
+void Pop(){
+    if(IsEmpty() == true){
+        printf("\nStack is empty. Underflow condition!");
+    }else{
+        --top;
+    }
+}
+ 
+ 
+int Top(int stack[]){
+    return stack[top];
+}
+ 
+int Size(){
+    return top + 1;
+}
